13ab02eb9fe31ad52656e1143016a66789154b96
RuslanGareev <rudhamanyu@gmail.com>
2022-11-28 17:11:20 +0500

    add Program

---
+++ Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array = { "123", "Символ", "VSCode", ":-)" };
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count++;        
+}
+
+string[] resultArr = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {        
+        resultArr[j] = array[i];
+        j++;
+    }
+}
